7c61af99e34ddd1806e65e2c52f908cab9b4646a
Sourabh Sikarwar <sourabhsikarwar1812@gmail.com>
2024-05-22 04:06:44 +0530

    Prompt changes

--- frontend/src/context/index.tsx
+++ frontend/src/context/index.tsx
@@ -38,7 +38,7 @@ const addAnswerToPrompt = (
 ) => {
   return `${prompt}Q${questionNumber + 1}: ${
     topic?.[level.toLowerCase()]?.[questionNumber].question
-  }\nAnswer: ${answer ? answer : "Not answered"}\n`;
+  }\n\nAnswer ${questionNumber + 1}: ${answer ? answer : "Not answered"}\n`;
 };
 
 const AppReducer = (state: AppState, action: any): AppState => {
